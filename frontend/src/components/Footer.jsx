56f04d27ec7341d39b929c4e1877bf71d15ab69c
huzaifa-asad <khuzaifaasad55@gmail.com>
2026-04-30 17:24:42 +0500

    added Dockerfile and docker-compose file

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,25 +4,25 @@ export default function Footer() {
       <p className="flex flex-col items-center gap-1">
         Developed with <span className="text-red-500">❤️</span> by{" "}
         <a
-          href="https://www.linkedin.com/in/rathan-kumar492"
+          href="https://www.linkedin.com/in/huzaifa-asad-dev"
           target="_blank"
           className="text-blue-600 hover:underline"
         >
-          Rathan Kumar
+          Huzaifa Asad
         </a>
       </p>
 
       <div className="flex flex-col items-center gap-1 mt-2">
         <a
-          href="mailto:developer.rathan@gmail.com"
+          href="mailto:teqtollc@gmail.com"
           className="text-blue-600 hover:underline"
         >
-          developer.rathan@gmail.com{" "}
+          teqtollc@gmail.com{" "}
         </a>
 
         <span className="flex items-center gap-1 text-gray-500 text-sm">
           <i className="bi bi-geo-alt-fill" style={{ fontSize: "20px" }}></i>
-          Mumbai, India
+          KP, Pakistan
         </span>
       </div>
     </footer>
